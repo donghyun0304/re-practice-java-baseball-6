41a8c002937980ee889d2d26feffc03748c01b78
donghyunkim <ghffu405@gmail.com>
2023-11-17 20:45:20 +0900

    feat: 상수 추가

--- src/main/java/baseball/constant/BaseballConstant.java
+++ src/main/java/baseball/constant/BaseballConstant.java
@@ -5,5 +5,8 @@ public class BaseballConstant {
     public static final int BASEBALL_LIST_MAX_NUMBER = 3;
     public static final int MIN_RANDOM_NUMBER = 1;
     public static final int MAX_RANDOM_NUMBER = 9;
+    public static final String BALL = "ball";
+    public static final String STRIKE = "strike";
+    public static final String NOTHING = "nothing";
 
 }
