5cdd8965835e35028fa2dcf095107254f48bfd50
donghyunkim <ghffu405@gmail.com>
2023-11-17 21:34:35 +0900

    refactor: 출력문 삭제

--- src/main/java/baseball/domain/BaseballResult.java
+++ src/main/java/baseball/domain/BaseballResult.java
@@ -1,10 +1,13 @@
 package baseball.domain;
 
+import baseball.constant.BaseballConstant;
+
 import java.util.Collections;
 import java.util.Map;
 
 public class BaseballResult {
 
+    private static final int STRIKE_NUMBER_FOR_END = 3;
     private Map<String, Integer> result;
 
     public BaseballResult(Map<String, Integer> result) {
@@ -13,5 +16,16 @@ public class BaseballResult {
 
     public Map<String, Integer> getResult() {
         return Collections.unmodifiableMap(result);
+    }
+
+    public boolean isPossibleEnd(){
+        if(getStrike() == STRIKE_NUMBER_FOR_END){
+            return true;
+        }
+        return false;
+    }
+
+    private int getStrike(){
+        return result.getOrDefault(BaseballConstant.STRIKE, 0);
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -12,8 +12,6 @@ public class InputView {
 
     public List<Integer> inputNumbers(){
         return getInput(() -> {
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine().trim();
             NumberValidator.validate(input);
             return Arrays.stream(input.split(""))
