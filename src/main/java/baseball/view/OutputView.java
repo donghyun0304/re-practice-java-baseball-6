7b75a6e2e9bdc402c2a924cb65645d7eb8ba71c2
donghyunkim <ghffu405@gmail.com>
2023-11-17 22:14:16 +0900

    feat: 결과출력 메소드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,8 +1,33 @@
 package baseball.view;
 
+import baseball.constant.BaseballConstant;
+import baseball.domain.BaseballResult;
+
+import java.util.Map;
+import java.util.Set;
+
+import static baseball.constant.BaseballConstant.*;
+
 public class OutputView {
 
     public void printStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printResult(BaseballResult baseballResult){
+        Map<String, Integer> result = baseballResult.getResult();
+
+        if(result.containsKey(BALL) && !result.containsKey(STRIKE)){
+            System.out.println(result.get(BALL) + "볼");
+        }
+        if(result.containsKey(BALL) && result.containsKey(STRIKE)){
+            System.out.println(result.get(BALL) + "볼 " + result.get(STRIKE) + "스트라이크");
+        }
+        if(!result.containsKey(BALL) && result.containsKey(STRIKE)){
+            System.out.println(result.get(STRIKE) + "스트라이크");
+        }
+        if(!result.containsKey(BALL) && !result.containsKey(STRIKE)){
+            System.out.println("낫싱");
+        }
     }
 }
