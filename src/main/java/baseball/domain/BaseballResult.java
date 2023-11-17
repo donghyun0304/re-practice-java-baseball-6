e1413545a269a3422f50907d66a8bebec818b2b8
donghyunkim <ghffu405@gmail.com>
2023-11-17 21:07:25 +0900

    feat: 결과 계산 메소드 추가

--- src/main/java/baseball/domain/BaseballResult.java
+++ src/main/java/baseball/domain/BaseballResult.java
@@ -1,9 +1,17 @@
 package baseball.domain;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class BaseballResult {
 
     private Map<String, Integer> result;
 
+    public BaseballResult(Map<String, Integer> result) {
+        this.result = result;
+    }
+
+    public Map<String, Integer> getResult() {
+        return Collections.unmodifiableMap(result);
+    }
 }
--- src/main/java/baseball/domain/WinningBaseballNumbers.java
+++ src/main/java/baseball/domain/WinningBaseballNumbers.java
@@ -2,17 +2,15 @@ package baseball.domain;
 
 import baseball.constant.BaseballConstant;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static baseball.constant.BaseballConstant.*;
 
-public class WinningBaseBallNumbers {
+public class WinningBaseballNumbers {
 
     private final List<Integer> numbers;
 
-    public WinningBaseBallNumbers(List<Integer> numbers) {
+    public WinningBaseballNumbers(List<Integer> numbers) {
         validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
@@ -22,5 +20,27 @@ public class WinningBaseBallNumbers {
         if(numbers.size() != BASEBALL_LIST_MAX_NUMBER){
             throw new IllegalArgumentException("[ERROR] 서로 다른 세개의 숫자가 아닙니다.");
         }
+    }
+
+    public BaseballResult calcResult(BaseballNumbers baseballNumbers){
+        List<Integer> tryNumbers = baseballNumbers.getNumbers();
+        Map<String, Integer> result = new HashMap<>();
+        for(int i=0; i< BASEBALL_LIST_MAX_NUMBER; i++){
+            for(int j=0; j<BASEBALL_LIST_MAX_NUMBER; j++){
+
+                if(numbers.get(i) == tryNumbers.get(i)){
+                    result.put(STRIKE, result.getOrDefault(STRIKE, 0) + 1);
+                    break;
+                }
+                if(numbers.contains(tryNumbers.get(j)) && numbers.get(i) != tryNumbers.get(i)){
+                    result.put(BALL, result.getOrDefault(BALL, 0) + 1);
+                    break;
+                }
+            }
+        }
+        if(result.isEmpty()){
+            result.put(NOTHING, 1);
+        }
+        return new BaseballResult(result);
     }
 }
