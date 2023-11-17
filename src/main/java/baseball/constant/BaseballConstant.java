0e92628e951feeb581dfe0b0976edc97d539dcdb
donghyunkim <ghffu405@gmail.com>
2023-11-17 16:28:12 +0900

    feat: 야구 상수 추가

--- src/main/java/baseball/constant/BaseballConstant.java
+++ src/main/java/baseball/constant/BaseballConstant.java
@@ -3,4 +3,7 @@ package baseball.constant;
 public class BaseballConstant {
 
     public static final int BASEBALL_LIST_MAX_NUMBER = 3;
+    public static final int MIN_RANDOM_NUMBER = 1;
+    public static final int MAX_RANDOM_NUMBER = 9;
+
 }
