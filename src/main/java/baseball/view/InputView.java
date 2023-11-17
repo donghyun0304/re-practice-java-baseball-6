1df2625b126058065d052387c905000b805e9ea5
donghyunkim <ghffu405@gmail.com>
2023-11-17 17:55:27 +0900

    feat: 숫자 입력 메소드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,32 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
 public class InputView {
 
+    public List<Integer> inputNumbers(){
+        return getInput(() -> {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine().trim();
+            return Arrays.stream(input.split(""))
+                    .map(number -> Integer.parseInt(number))
+                    .collect(Collectors.toList());
+        });
+    }
+
+    private <T> T getInput(Supplier<T> inputSupplier){
+        while(true){
+            try{
+                return inputSupplier.get();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
